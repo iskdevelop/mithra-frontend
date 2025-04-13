affbec0c8a0170a8f6f380c273cb8540c80a5682
Kid A <thisismeamir@outlook.com>
2025-04-13 19:35:34 +0330

    [implementation] Glass Component

---
+++ components/index/GlassSection.vue
@@ -0,0 +1,29 @@
+
+<template>
+     <div class="flex
+     data-[orientation=vertical]:flex-col
+     data-[orientation=horizontal]:flex-row
+     gap-16px
+     text-[--text-on-color]
+     hover:text-[--text-primary]
+     bg-[--background-selected]
+     hover:bg-[--background]
+      justify-center
+     h-full w-full
+     nu-transition-expressive-standard-moderate-02
+     xs:p-8px sm:p-16px md:p-32px lg:p-64px xl:p-96px"
+     data-orientation="vertical">
+          <slot/>
+     </div>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+     title: string
+     description: string
+     orientation: 'horizontal' | 'vertical'
+}>()
+</script>
+
+<style scoped lang="scss">
+
+</style>
